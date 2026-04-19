656c65ffeb1eabf6f876e258d0fb1158990a1c33
Leormar <LORJUELA7@GMAIL.COM>
2026-04-19 04:40:19 +0000

    add lentes data file

---
+++ app/data/lentes.js
@@ -0,0 +1,39 @@
+export const LENTES = {
+  "Full Range / Trifocal": [
+    "Alcon PanOptix (TFNT00)",
+    "Johnson & Johnson Tecnis Synergy",
+    "Zeiss AT LISA tri 839MP",
+    "Hoya Vivinex iSert XY1A",
+    "Bausch & Lomb LuxSmart",
+    "PhysIOL FineVision POD F",
+    "Carl Zeiss AT TRIEDY",
+    "Rayner RayOne Trifocal"
+  ],
+  "EDOF (Extended Depth of Focus)": [
+    "Johnson & Johnson Tecnis Eyhance",
+    "Alcon Vivity (DFT015)",
+    "Bausch & Lomb LuxGood",
+    "Zeiss AT LARA 829MP",
+    "Rayner RayOne EMV",
+    "Hoya Vivinex Impress",
+    "PhysIOL MiniWell",
+    "SAV IOL SAV-IOL Plus"
+  ],
+  "Plus / Bifocal": [
+    "Johnson & Johnson Tecnis Multifocal +2.75",
+    "Johnson & Johnson Tecnis Multifocal +3.25",
+    "Alcon ReSTOR +2.5",
+    "Alcon ReSTOR +3.0",
+    "Zeiss AT LISA 809M",
+    "Bausch & Lomb Crystalens AO",
+    "PhysIOL FineVision POD F GF",
+    "Hanita Bi-Flex M"
+  ],
+  "Monofocal premium": [
+    "Alcon AcrySof IQ (SA60AT)",
+    "Johnson & Johnson Tecnis 1-piece",
+    "Zeiss CT ASPHINA 409MP",
+    "Bausch & Lomb enVista",
+    "Hoya Vivinex iSert XY1"
+  ]
+}
