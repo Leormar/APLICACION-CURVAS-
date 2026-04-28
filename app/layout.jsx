1315269f77e694456e256d2fed623f12d00cafc1
Leormar <LORJUELA7@GMAIL.COM>
2026-04-27 20:46:31 -0500

    Integrate Google Translate in layout.jsx
    Added Google Translate integration to the layout.

--- app/layout.jsx
+++ app/layout.jsx
@@ -25,6 +25,17 @@ export default function RootLayout({ children }) {
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="CurvasIOL" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit" async></script>
+<script dangerouslySetInnerHTML={{ __html: `
+  function googleTranslateElementInit() {
+    new google.translate.TranslateElement({
+      pageLanguage: 'es',
+      includedLanguages: 'en,it,ca,fr,es',
+      autoDisplay: true,
+      layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+    }, 'google_translate_element');
+  }
+` }} />
       </head>
       <body>
         <SessionProvider>{children}</SessionProvider>
